f850058ee8fb2a0df5b0819349657975c89fc42e
Theo Armour <t.armour@gmail.com>
2016-09-20 11:41:27 -0700

    elev view ter fix placard toggles

--- elevations-core3/d-ter-terrain/terrain-r1.js
+++ elevations-core3/d-ter-terrain/terrain-r1.js
@@ -26,9 +26,9 @@
 
 					'<input type=checkbox id=TERchkRotate onchange=THR.controls.autoRotate=TERchkRotate.checked > scene rotation' + b +
 
-					'<input type=checkbox id=TERchPlacards onchange=THR.placards.visible=!THR.placards.visible; > Messages' + b +
+					'<input type=checkbox id=TERchPlacards onchange=PLA.placards.visible=!PLA.placards.visible; > Messages' + b +
 
-					'<input type=checkbox id=TERchPlacards onchange=THR.nearby.visible=!THR.nerby.visible; > earby Pllaces' + b +
+					'<input type=checkbox id=TERchPlacards onchange=PLA.nearby.visible=!PLA.nearby.visible; > Nearby Places' + b +
 
 					'<input type=checkbox id=TERchkFog onchange=THR.toggleFog(this.checked); checked > Fog' + b +
 
